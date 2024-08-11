c216f461f3eb0a631c4b10d9117707d4c410e74b
Adrian Talbot <adrianrtalbot@me.com>
2024-08-11 11:44:18 +0100

    Add API_KEY and extract it when the initial view is displayed Add button to bottom of ContentView.

--- Football Challenge/ContentView.swift
+++ Football Challenge/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var apiKey: String? = nil
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Text("Your API_KEY is: \(apiKey ?? "nil")")
+            
+            Spacer()
+            Button("Click for teams") {
+                getTeams()
+            }
         }
         .padding()
+        .onAppear(perform: {
+            apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+        })
+    }
+    
+    func getTeams() {
+        print("Not implemented yet")
     }
 }
 
