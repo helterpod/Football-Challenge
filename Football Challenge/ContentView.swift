6b821e5817bd5a6dce5a10f3ebcea5c52cfa9579
Adrian Talbot <adrianrtalbot@me.com>
2024-08-11 11:14:23 +0100

    Initial Commit

---
+++ Football Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football Challenge
+//
+//  Created by Adrian Richard Talbot on 11/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football Challenge/Football_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_ChallengeApp.swift
+//  Football Challenge
+//
+//  Created by Adrian Richard Talbot on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Football_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
